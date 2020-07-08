1bcdc5a5131d0c726bf52e3da4a6096decd3364f
Assaad El Halabi <ahe34@mail.aub.edu>
2020-07-08 23:35:52 +0300

    First commit

---
+++ src/build/precache-manifest.a3e6d2895f6014029966ebe92c805cf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf7e62cf8a61c6994af43a0243de821",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abbc11ed256ffecc7573",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "c5b97bf2a931a128f6ed",
+    "url": "/static/js/2.aef446bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aef446bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abbc11ed256ffecc7573",
+    "url": "/static/js/main.1f5582de.chunk.js"
+  },
+  {
+    "revision": "1209add7631694ddf8b1",
+    "url": "/static/js/runtime-main.3dc933a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
